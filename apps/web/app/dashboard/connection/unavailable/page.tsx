e07894857dfae21b78fadacccb2ab082ee24aeca
Geektech <development.geektech@gmail.com>
2026-03-24 15:39:25 +0530

    worked on chat module

--- apps/web/app/dashboard/connection/unavailable/page.tsx
+++ apps/web/app/dashboard/connection/unavailable/page.tsx
@@ -10,35 +10,26 @@ const Matchprofile = () => {
       <Card className="md:d-block md:bg-[url('/images/bg_auth_center.png')] py-0 bg-no-repeat bg-cover bg-center w-full max-w-[620px] md:shadow-[0px_4px_4px_0px_#00000014] bg-transparent overflow-hidden   md:backdrop-blur-xl border-0 md:border md:border-white/10 rounded-2xl md:shadow-2xl">
       <CardContent className="flex flex-col gap-2 sm:p-10 sm:px-6 px-0 text-left mt-[3rem] h-[80vh] sm:h-auto">
           <div className="flex items-center justify-center">
-            
             <Image
-              src="/images/match-pro-1.png"
+              src="/images/unavailable.png"
               alt="connection"
-              width={130}
-              height={130}
-              className='border-4 border-solid border-[#C39936] rounded-full'
-            />
-            <div className="w-full h-[2px] bg-gradient-to-r from-[#663EA2] to-[#1D073C]"></div>
-            <Image
-              src="/images/match-pro-2.png"
-              alt="connection"
-              width={130}
-              height={130}
-              className='border-4 border-solid border-[#C39936] rounded-full'
+              width={92}
+              height={92}
+              className=''
             />
           </div>
           <div className="flex flex-col gap-2 items-center justify-center sm:mt-[60px] mt-[20px]">
             <h2 className="text-white text-[24px] font-normal font-serif">
-              It’s a Meaningful Connection
+              This Profile Is Unavailable
             </h2>
             <p className="text-white text-[14px] font-light text-center sm:mb-[32px] mb-[20px] max-w-[400px]">
-              You both expressed interest.
+            This member is no longer accessible. They may have deactivated their account or changed their privacy settings.
             </p>
             <Button className="cursor-pointer w-full text-base h-[52px] mt-[12px] rounded-[12px] md:rounded-[8px] bg-gradient-to-r from-[#c58b00] via-[#f5d76e] to-[#c58b00] text-[#913C01] font-semibold hover:opacity-90 transition disabled:opacity-60">
-              Begin Conversation
+              Return to Discovery
             </Button>
             <button className="cursor-pointer w-auto text-base h-auto mt-[12px] rounded-[12px] md:rounded-[8px]  text-[#913C01] font-semibold  transition">
-              Keep Exploring
+              Contact Support
             </button>
           </div>
         </CardContent>
